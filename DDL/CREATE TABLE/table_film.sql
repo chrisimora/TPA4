718e6dd13212191ae885746fd08e3af43e8d16da
chrisimora <70998302+chrisimora@users.noreply.github.com>
2022-11-14 21:55:47 +0700

    Membuat Table Film

---
+++ DDL/CREATE TABLE/table_film.sql
@@ -0,0 +1,8 @@
+CREATE TABLE film
+  (
+     id_film       INT auto_increment PRIMARY KEY,
+     nama          VARCHAR(255) NULL,
+     tanggal_rilis DATE NULL,
+     harga         INT NULL,
+     durasi        INT NULL
+  ); 
